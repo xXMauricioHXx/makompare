5a1f69bee03220e2c7c31b2363fa05d34a991324
mauricio.henrique <mauricio.henrique@4all.com>
2019-09-06 12:54:02 -0300

    Concluido a tela de home

---
+++ src/components/Banner.vue
@@ -0,0 +1,67 @@
+<template>
+  <header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
+        <Title class="mb-0">Procure. Compare. Compre.</Title>
+        <form class="search-form">
+          <div class="form-group">
+            <input class="form-control form-control-lg" type="text" placeholder="Informe o produto" />
+            <label class="material-icons">search</label>
+          </div>
+        </form>
+        <!--<a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>-->
+      </div>
+    </div>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+  </header>
+</template>
+
+<script>
+import Title from "./Title";
+export default {
+  name: "Banner",
+  components: {
+    Title
+  }
+};
+</script>
+
+<style scoped>
+@media (max-width: 575.98px) {
+  .search-form {
+    width: 90%;
+    margin: 10px auto;
+  }
+}
+.search-form {
+  width: 80%;
+  margin: 30px auto;
+  position: relative;
+}
+.search-form label {
+  color: #6610f2;
+  position: absolute;
+  top: 2px;
+  right: 10px;
+  padding: 11px;
+  border-left: 1px solid #e8e7e9;
+  height: 40px;
+  font-weight: bold;
+}
+
+.search-form label:hover {
+  cursor: pointer;
+}
+
+.search-form input {
+  border-radius: 1.3rem;
+}
+.search-form input:focus {
+  border-color: #6610f2;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgba(174, 0, 255, 0.25);
+}
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="mt-5">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-12 order-lg-2">
+          <div class="p-5">
+            <Title class="mb-3">Produtos mais procurados</Title>
+            <div class="bd-example">
+              <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="row">
+                      <Card v-for="product in products" :product="product" v-bind:key="product.id" />
+                    </div>
+                  </div>
+                </div>
+                <a
+                  class="carousel-control-prev"
+                  href="#carouselExampleCaptions"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span
+                    class="material-icons courousel-arrow courousel-arrow-left"
+                  >keyboard_arrow_left</span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a
+                  class="carousel-control-next"
+                  href="#carouselExampleCaptions"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span
+                    class="material-icons courousel-arrow courousel-arrow-right"
+                  >keyboard_arrow_right</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import Title from "./Title";
+import Card from "./Card";
+
+export default {
+  name: "Carousel",
+  components: {
+    Title,
+    Card
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: "SMASHBOX",
+          description: "Batom Líquido Smashbox - Always On",
+          price: "R$ 125,00",
+          image:
+            "https://epocacosmeticos.vteximg.com.br/arquivos/ids/327787-300-300/big-spender.jpg?v=636886750756870000"
+        },
+        {
+          id: 2,
+          title: "SMASHBOX",
+          description: "Batom Líquido Smashbox - Always On",
+          price: "R$ 125,00",
+          image:
+            "https://epocacosmeticos.vteximg.com.br/arquivos/ids/327787-300-300/big-spender.jpg?v=636886750756870000"
+        },
+        {
+          id: 3,
+          title: "SMASHBOX",
+          description: "Batom Líquido Smashbox - Always On",
+          price: "R$ 125,00",
+          image:
+            "https://epocacosmeticos.vteximg.com.br/arquivos/ids/327787-300-300/big-spender.jpg?v=636886750756870000"
+        },
+        {
+          id: 4,
+          title: "SMASHBOX",
+          description: "Batom Líquido Smashbox - Always On",
+          price: "R$ 125,00",
+          image:
+            "https://epocacosmeticos.vteximg.com.br/arquivos/ids/327787-300-300/big-spender.jpg?v=636886750756870000"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.courousel-arrow-right {
+  margin-left: 195px;
+}
+
+.courousel-arrow-left {
+  margin-right: 195px;
+}
+
+.courousel-arrow {
+  color: #6610f2;
+  font-size: 3rem !important;
+}
+.brand-color {
+  color: #fd7e14;
+}
+</style>
