e128877dbbd324cffe5e25037951b0c52dfc4e42
xXMauricioHXx <mauriciosh558@gmail.com>
2019-11-02 11:07:25 -0300

    make it better

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,8 +3,13 @@
     <div class="masthead-content">
       <div class="container">
         <Title class="mb-0">Procure. Compare. Compre.</Title>
-        <AutoComplete link="/product-list?product" :search="search"></AutoComplete>
-        <!--<a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>-->
+        <form v-on:submit.prevent="submit" class="search-form">
+          <div class="form-group">
+            <input class="form-control form-control-lg" type="text" v-model="product" />
+
+            <label class="material-icons" v-on:click="submit">search</label>
+          </div>
+        </form>
       </div>
     </div>
     <div class="bg-circle-1 bg-circle"></div>
@@ -16,50 +21,67 @@
 
 <script>
 import Title from "./Title";
-import AutoComplete from "./AutoComplete";
 import { searchProducts } from "../services/makompare";
 
 export default {
   name: "Banner",
   components: {
-    Title,
-    AutoComplete
+    Title
   },
   data() {
     return {
-      product: "",
-      products: ["Batom", "Pó"]
+      product: ""
     };
   },
   methods: {
-    search: async input => {
-      // console.log(input);
-      // if (input.length < 1) {
-      //   return [];
-      // }
-
-      // const products = await searchProducts(input);
-      // return products.map(product => product.name);
-      return [
-        "Batom 1",
-        "Batom 2",
-        "Batom 3",
-        "Batom 4",
-        "Batom 5",
-        "Batom 6",
-        "Batom 7",
-        "Batom 8"
-      ];
-    },
-    handleSubmit(result) {
-      console.log(result);
+    submit() {
+      this.$router.push(`/product-list?product=${this.product}`);
     }
   }
 };
 </script>
 
 <style scoped>
-header {
-  height: 100vh;
+.search-form {
+  width: 80%;
+  margin: 30px auto;
+  position: relative;
+}
+
+.search-form label {
+  color: #6610f2;
+  position: absolute;
+  top: 2px;
+  right: 10px;
+  padding: 11px;
+  border-left: 1px solid #e8e7e9;
+  height: 40px;
+  font-weight: bold;
+}
+
+.search-form label:hover {
+  cursor: pointer;
+}
+
+.search-form input {
+  border-radius: 1.3rem;
+}
+.search-form input:focus {
+  border-color: #6610f2;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgba(174, 0, 255, 0.25);
+}
+
+.autocomplete-result-mkc {
+  color: black !important;
+  text-align: left !important;
+  z-index: 99999999999 !important;
+}
+
+@media (max-width: 575px) {
+  .search-form {
+    width: 100%;
+    margin: 10px auto;
+  }
 }
 </style>
